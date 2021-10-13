cea179a5097375a6b0613b24caf0080360a6b5c9
Damhanlk <damhan2611@gmail.com>
2021-10-13 16:09:07 +0000

    Add Logout functionality

--- app.py
+++ app.py
@@ -88,11 +88,22 @@ def login():
 
 @app.route("/profile/<username>", methods=["GET", "POST"])
 def profile(username):
-        # take session users's username from the database
+        """ take session users's username from the database """
         username = mongo.db.users.find_one(
             {"username": session["user"]})["username"]
-        return render_template("profile.html", username=username)
 
+        if session["user"]:
+            return render_template("profile.html", username=username)
+
+        return redirect(url_for("login"))
+
+
+@app.route("/logout")
+def logout():
+    """ remove user from session cookies """
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
