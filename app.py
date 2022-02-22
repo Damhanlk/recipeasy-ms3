8a54d05510832c7f07d40bd1fd90a590e7ab36cf
Damhanlk <damhan2611@gmail.com>
2022-02-22 21:02:58 +0000

    Add Log In function

--- app.py
+++ app.py
@@ -40,3 +40,32 @@ def home():
                            username=get_user(),
                            acc_type=get_acc_type())
 
+
+# Log In function
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check if user already exists in database
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+       
+        if existing_user:
+            # Check if hashed password matches user input
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome, {}".format(
+                        request.form.get("username")))
+                    return redirect(url_for(
+                        "profile", username=session["user"]))
+            else:
+                # Password is invalid
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            # If username does not exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
